bdcf2e850b76b13d7b5b3214153d74b1beaa5c25
Igorrosiak <pedro_torques@estudante.sesisenai.org.br>
2022-04-12 12:23:46 -0300

    aula 12/04

---
+++ exercicios_switch/js/case.js
@@ -0,0 +1,18 @@
+function search_price(opcao) {
+    switch (opcao) {
+        case "uva":
+            document.getElementById("produto").innerText = "Preço da uva: R$3,89" 
+            break;
+        case "laranja":
+            document.getElementById("produto").innerText = "Preço da laranja: R$1,70" 
+            break;
+        case "pera":
+            document.getElementById("produto").innerText = "Preço da pera: R$2,50" 
+            break;
+        default:
+            document.getElementById("produto").innerText = "Produto não disponível" 
+            break;
+    }
+
+    document.getElementById("div_produto").style.display = "block"
+}
