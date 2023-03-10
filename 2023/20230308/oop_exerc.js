e88cde160ebeaa48263d815e716be3be720ef812
PEDRO LUÍS RICKLI TORQUES <pedro_torques@estudante.sesisenai.org.br>
2023-03-10 11:11:20 -0300

    q7 terminada

--- 2023/20230308/oop_exerc.js
+++ 2023/20230308/oop_exerc.js
@@ -75,12 +75,14 @@ let numeroConta = 123456789
 let nomeConta = "Carlos Amaral"
 let saldoInicial = true
 let valorSaldo = 650
+let valorDeposito = 220
+let valorSaque = 100
 
 class Conta {
-    constructor(numero, nome, deposito) {
+    constructor(numero, nome, valor) {
         this.numero = numero
         this.nome = nome
-        this.deposito = deposito
+        this.valor = valor
     }
 }
 
@@ -90,10 +92,31 @@ let criarConta = function() {
     "\nHaverá depósito inicial (S/N)? " + saldoInicial + 
     "\nValor do depósito: " + valorSaldo
     );
-    if (saldoInicial == false) valorSaldo = 0
-
-    const conta1 = new Conta(numeroConta, nomeConta, valorSaldo)
+    if (saldoInicial == false || valorSaldo < 0) valorSaldo = 0
 }
 
 criarConta()
 
+const conta1 = new Conta(numeroConta, nomeConta, valorSaldo)
+
+console.log(`\nDados da conta: \nNúmero da conta: ${conta1.numero}, Titular: ${conta1.nome}, Saldo: R$${(conta1.valor).toFixed(2)}`);
+
+
+
+let deposito = function() {
+    console.log("\nQuanto deseja depositar? " + valorDeposito);
+    valorDeposito > 0 ? conta1.valor += valorDeposito : console.log("Valor não pode ser menor do que zero.");
+
+    console.log(`Dados da conta: \nNúmero da conta: ${conta1.numero}, Titular: ${conta1.nome}, Saldo: ${(conta1.valor).toFixed(2)}`);
+}
+
+deposito()
+
+let saque = function() {
+    console.log("\nQuanto deseja sacar? " + valorSaque);
+    valorSaque > 0 ? conta1.valor -= (valorSaque + 5) : console.log("Valor não pode ser menor do que zero.");
+
+    console.log(`Dados da conta: \nNúmero da conta: ${conta1.numero}, Titular: ${conta1.nome}, Saldo: ${(conta1.valor).toFixed(2)}`);
+}
+
+saque()
